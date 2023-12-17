10a0aaf139254abe7593438b7832c5150052e046
Saket-Kumar-Singh <saketkrsingh37772@gmail.com>
2023-12-17 13:45:59 +0530

    First Commit

---
+++ FirstProgram.cbl
@@ -0,0 +1,35 @@
+      ******************************************************************
+      * Author: SAKET-KUMAR-SINGH
+      * Date: 12-17-2023
+      * Purpose: WORK-WITH-REDEFINES
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. WORK_WITH_REDIFINE.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+      * 77  WS-DATE PIC X(10).
+       01  A.
+         02  X1 PIC 99.
+         02  X2 REDEFINES X1 PIC X(2).
+         02  X3 PIC X(15) VALUE ALL '*'.
+
+       01  Z REDEFINES A.
+         02  Z1 PIC X(1).
+         02  Z2 PIC X(16).
+
+       01  B.
+         02  Y1 PIC X VALUE 'B'.
+         02  Y2 PIC X(5) VALUE ALL '$'.
+         02  Y3 PIC X(10) VALUE ALL '*'.
+
+       01  C REDEFINES B.
+         02   Z1 PIC X.
+         02   Z2 PIC X(16).
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY B.
+            STOP RUN.
+       END PROGRAM WORK_WITH_REDIFINE.
